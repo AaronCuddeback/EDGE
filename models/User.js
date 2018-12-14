172f6308f02c7570649481f9feab0fe3ab6b74ca
Aaron Cuddeback <aaron.cuddeback@outlook.com>
2018-12-13 20:50:57 -0800

    add virtual gravatar, logout middleware,  and user account edit screen

--- models/User.js
+++ models/User.js
@@ -23,6 +23,12 @@ const userSchema = new Schema({
   }
 });
 
+userSchema.virtual('gravatar').get(function() {
+  const hash = md5(this.email);
+  return `https://gravatar.com/avatar/${hash}?s=200`;
+  // return `https://scontent.fsac1-2.fna.fbcdn.net/v/t1.0-9/16939694_627536214120934_4815463030253037492_n.jpg?_nc_cat=107&_nc_ht=scontent.fsac1-2.fna&oh=1776b17f9e91f376ff318cf75a81914c&oe=5CA906CD`;
+});
+
 userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
 userSchema.plugin(mongodbErrorHandler);
 
--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
-router.get('/add', storeController.addStore);
+router.get('/add', authController.isLoggedIn, storeController.addStore);
 
 router.post(
   '/add',
@@ -31,6 +31,7 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+router.post('/login', authController.login);
 router.get('/register', userController.registerForm);
 
 // validate the registration data
@@ -42,5 +43,9 @@ router.post(
   userController.register,
   authController.login
 );
+
+router.get('/logout', authController.logout);
+router.get('/account', authController.isLoggedIn, userController.account);
+router.post('/account', catchErrors(userController.updateAccount));
 
 module.exports = router;
